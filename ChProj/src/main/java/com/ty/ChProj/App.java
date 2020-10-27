b5e4f3b5bec24208e1b8afec2dd3b52ad8789438
prakashabkgowda <bk.prakasha4@gmail.com>
2020-10-27 13:31:11 +0530

    second cmt

---
+++ ChProj/src/main/java/com/ty/ChProj/App.java
@@ -0,0 +1,32 @@
+package com.ty.ChProj;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+   @Test
+   public void test()
+   
+   
+   {
+	   System.out.println("test one is executing"); 
+	  
+   }
+   
+   @Test
+   public void test2()
+   
+   
+   {
+	   System.out.println("test two is executing"); 
+	  
+   }
+   
+}
---
+++ ChProj/src/main/java/com/ty/ChProj/B.java
@@ -0,0 +1,13 @@
+package com.ty.ChProj;
+
+import org.testng.annotations.Test;
+
+public class B {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("testing");
+	}
+
+}
